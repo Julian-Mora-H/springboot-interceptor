08045d713e0bb3a4154233bd513f92aaff865629
Julian-Mora <felipmora43@gmail.com>
2026-08-04 21:49:17 -0500

    Create proyect " Use Interceptor before or after in Method Http "

---
+++ src/main/java/com/julian/curso/springboot/app/interceptor/springboot_interceptor/interceptors/LoadingTimeInterceptor.java
@@ -0,0 +1,27 @@
+package com.julian.curso.springboot.app.interceptor.springboot_interceptor.interceptors;
+
+import org.jspecify.annotations.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class LoadingTimeInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+            @Nullable ModelAndView modelAndView) throws Exception {
+        
+        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        return HandlerInterceptor.super.preHandle(request, response, handler);
+    }
+    
+}
